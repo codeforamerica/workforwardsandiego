7e0e3bc4a6b50a858411ec949b930e79a74818ed
Paras Sanghavi <paras@codeforamerica.org>
2017-03-29 10:18:58 -0700

    Use db object, use integers for booleans

--- services/membership_application_service.rb
+++ services/membership_application_service.rb
@@ -7,18 +7,19 @@ module WorkForwardNola
 
     def initialize(params)
       @params = params
+      @form_data = {}
     end
 
     def get_filled_form
-      JobApp.create(
+      job_app = JobApp.create(
           email: @params[:email],
           last_name: @params[:last_name],
           first_name: @params[:first_name],
           gender: @params[:gender],
           phone: @params[:phone],
           selective_service: @params[:selective_service],
-          recently_laid_off: true?(@params[:recently_laid_off]),
-          veteran: true?(@params[:veteran]),
+          recently_laid_off: @params[:recently_laid_off],
+          veteran: @params[:veteran],
           work_authorization: @params[:work_authorization],
           education: @params[:education],
           current_school: @params[:current_school],
@@ -30,118 +31,96 @@ module WorkForwardNola
           wage: @params[:wage],
           hours_worked: @params[:hours_worked],
           date_last_worked: @params[:date_last_worked],
-          farm_work: true?(@params[:farm_work]),
+          farm_work: @params[:farm_work],
           termination_notice: @params[:termination_notice],
-          looking_for_work: true?(@params[:looking_for_work]),
+          looking_for_work: @params[:looking_for_work],
           desired_job: @params[:desired_job],
-          military_caregiver: true?(@params[:military_caregiver]),
-          military: true?(@params[:military]),
-          military_dependent: true?(@params[:military_dependent]),
-          tanf: true?(@params[:tanf]),
-          snap: true?(@params[:snap]),
-          general_assistance: true?(@params[:general_assistance]),
-          refugee_cash_assistance: true?(@params[:refugee_cash_assistance]),
-          expungement: true?(@params[:expungement]),
-          case_manager: true?(@params[:case_manager]),
+          military_caregiver: @params[:military_caregiver],
+          military: @params[:military],
+          military_dependent: @params[:military_dependent],
+          tanf: @params[:tanf],
+          snap: @params[:snap],
+          general_assistance: @params[:general_assistance],
+          refugee_cash_assistance: @params[:refugee_cash_assistance],
+          expungement: @params[:expungement],
+          case_manager: @params[:case_manager],
           services: @params[:services],
           other: @params[:other_services]
       )
 
-      form_data = {
-          Email: @params[:email],
-          'Last name Family name  surname': @params[:last_name],
-          'First name Given name': @params[:first_name],
-          'Gender identity': @params[:gender],
-          'Primary phone': @params[:phone],
-          @params[:selective_service] => 'Yes',
-          @params[:work_authorization] => 'Yes',
-          @params[:education] => 'Yes',
-          @params[:current_employment_status] => 'Yes',
-          @params[:unemployment_insurance] => 'Yes',
-          'Total number of individuals living in your household': @params[:household_size],
-          'Total income you earned within last 6 months': @params[:income],
-          @params[:termination_notice] => 'Yes',
-          'What is your desired job': @params[:desired_job],
+      @form_data = {
+          Email: job_app.email,
+          'Last name Family name  surname': job_app.last_name,
+          'First name Given name': job_app.first_name,
+          'Gender identity': job_app.gender,
+          'Primary phone': job_app.phone,
+          job_app.selective_service => 'Yes',
+          job_app.work_authorization => 'Yes',
+          job_app.education => 'Yes',
+          job_app.current_employment_status => 'Yes',
+          job_app.unemployment_insurance => 'Yes',
+          'Total number of individuals living in your household': job_app.household_size,
+          'Total income you earned within last 6 months': job_app.six_month_income,
+          job_app.termination_notice => 'Yes',
+          'What is your desired job': job_app.desired_job,
           Date: Date.today
       }
 
-      form_data['Layoff transition support'] = 'Yes' if @params[:recently_laid_off] == 'true'
+      @form_data['Layoff transition support'] = 'Yes' if job_app.recently_laid_off
 
-      form_data["Veterans' resources"] = 'Yes' if @params[:veteran] == 'true'
+      @form_data["Veterans' resources"] = 'Yes' if job_app.veteran
 
-      form_data[@params[:current_school]] = 'Yes' if @params[:current_school] != 'no'
+      @form_data[job_app.current_school] = 'Yes' if job_app.current_school != 'no'
 
-      if ['employed', 'employed with notice of military separation', 'employed with notice of termination'].include? @params[:current_employment_status]
-        form_data['Employer'] = @params[:employer]
-        form_data['Hourly wage'] = @params[:wage]
-        form_data['Hours worked'] = @params[:hours_worked]
+      if ['employed', 'employed with notice of military separation', 'employed with notice of termination'].include?(job_app.current_employment_status)
+        @form_data['Employer'] = job_app.employer
+        @form_data['Hourly wage'] = job_app.wage
+        @form_data['Hours worked'] = job_app.hours_worked
       end
 
       if @params[:current_employment_status] == 'not employed'
-        form_data['Last employer'] = @params[:employer]
-        form_data['Hourly wage_2'] = @params[:wage]
-        form_data['Date last worked'] = @params[:date_last_worked]
+        @form_data['Last employer'] = job_app.employer
+        @form_data['Hourly wage_2'] = job_app.wage
+        @form_data['Date last worked'] = job_app.date_last_worked
       end
 
-      form_data['Yes'] = 'Yes' if @params[:farm_work] == 'true'
-      form_data['No'] = 'Yes' if @params[:farm_work] == 'false'
+      fill_boolean_field(job_app.farm_work, 'Yes', 'No')
+      fill_boolean_field(job_app.looking_for_work, 'are you currently looking for work: Yes','are you currently looking for work: No')
+      fill_boolean_field(job_app.military_caregiver, 'yes1','no1')
+      fill_boolean_field(job_app.military, 'yes3','no3')
+      fill_boolean_field(job_app.military_dependent, 'yes4','no4')
+      fill_boolean_field(job_app.tanf, 'yes5','no5')
+      fill_boolean_field(job_app.snap, 'yes6','no6')
+      fill_boolean_field(job_app.general_assistance, 'yes7','no7')
+      fill_boolean_field(job_app.refugee_cash_assistance, 'yes8','no8')
 
-      form_data['are you currently looking for work: Yes'] = 'Yes' if @params[:looking_for_work] == 'true'
-      form_data['are you currently looking for work: No'] = 'Yes' if @params[:looking_for_work] == 'false'
-
-      form_data['yes1'] = 'Yes' if @params[:military_caregiver] == 'true'
-      form_data['no1'] = 'Yes' if @params[:military_caregiver] == 'false'
-
-      form_data['yes3'] = 'Yes' if @params[:military] == 'true'
-      form_data['no3'] = 'Yes' if @params[:military] == 'false'
-
-      form_data['yes4'] = 'Yes' if @params[:military_dependent] == 'true'
-      form_data['no4'] = 'Yes' if @params[:military_dependent] == 'false'
-
-      form_data['yes5'] = 'Yes' if @params[:tanf] == 'true'
-      form_data['no5'] = 'Yes' if @params[:tanf] == 'false'
-
-      form_data['yes6'] = 'Yes' if @params[:snap] == 'true'
-      form_data['no6'] = 'Yes' if @params[:snap] == 'false'
-
-      form_data['yes7'] = 'Yes' if @params[:general_assistance] == 'true'
-      form_data['no7'] = 'Yes' if @params[:general_assistance] == 'false'
-
-      form_data['yes8'] = 'Yes' if @params[:refugee_cash_assistance] == 'true'
-      form_data['no8'] = 'Yes' if @params[:refugee_cash_assistance] == 'false'
-
-      form_data['Expungement'] = 'Yes' if @params[:expungement] == 'true'
-
-      form_data['Support of case manager'] = 'Yes' if @params[:case_manager] == 'true'
+      @form_data['Expungement'] = 'Yes' if job_app.expungement
+      @form_data['Support of case manager'] = 'Yes' if job_app.case_manager
 
       if @params[:services]
         @params[:services].each do |val|
-          form_data[val] = 'Yes'
+          @form_data[val] = 'Yes'
         end
       end
 
-      if @params[:other_services]
-        form_data['Other (Please explain)'] = 'Yes'
+      if job_app.other
+        @form_data['Other (Please explain)'] = 'Yes'
 
-        length = @params[:other_services].length
-        form_data['Other Please explain 1'] = @params[:other_services][0..(length/2)]
-        form_data['Other Please explain 2'] = @params[:other_services][(length/2 + 1)..length]
+        length = job_app.other.length
+        @form_data['Other Please explain 1'] = job_app.other[0..(length/2)]
+        @form_data['Other Please explain 2'] = job_app.other[(length/2 + 1)..length]
       end
 
       filename = "/tmp/#{SecureRandom.urlsafe_base64}.pdf"
 
-      PdfForms.new('pdftk').fill_form PDF_PATH, filename, form_data
+      PdfForms.new('pdftk').fill_form PDF_PATH, filename, @form_data
 
       filename
     end
 
-    def true?(i)
-      if i == 'true'
-        true
-      elsif i == 'false'
-        false
-      else
-        raise "#{i} is not a boolean"
+    def fill_boolean_field(param, yes_key, no_key)
+      unless param.nil?
+        param ? @form_data[yes_key] = 'Yes' : @form_data[no_key] = 'Yes'
       end
     end
   end
